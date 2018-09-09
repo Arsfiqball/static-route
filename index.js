4c8b3c21c3b0cb2606dba90558a79abd7293bf44
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2018-09-09 08:58:55 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs-extra')
+const path = require('path')
+
+function serve (cb, opts) {
+  const server = http.createServer((req, res) => {
+    const parsedURL = url.parse(req.url)
+
+    const ctx = {
+      send: body => {
+        res.end(body, 'utf8')
+      }
+    }
+
+    const router = (pathname, controller) => {
+      if (pathname === parsedURL.pathname) controller(ctx)
+    }
+
+    cb(router)
+
+    res.end('Not found', 'utf8')
+  })
+
+  server.listen(3000, () => {
+    console.log('server is listening on port 3000')
+  })
+}
+
+function compile (cb, opts) {
+  const router = (pathname, controller) => {
+    const outputDir = opts && opts.output
+      ? path.resolve(opts.output + pathname)
+      : path.resolve(process.cwd(), 'dist' + pathname)
+
+    const ctx = {
+      send: body => {
+        fs.writeFile(outputDir + '/index.html', body, 'utf8')
+          .then(() => console.log('Published ' + pathname))
+      }
+    }
+
+    fs.ensureDir(outputDir)
+      .then(() => controller(ctx))
+  }
+
+  cb(router)
+}
+
+module.exports = function (mode, cb, opts) {
+  if (mode === 'compile') return compile(cb, opts)
+  if (mode === 'serve') return serve(cb, opts)
+}
